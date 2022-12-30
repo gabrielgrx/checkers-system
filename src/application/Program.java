037e3f30040885b138d976f051cf16223d3c2ebf
Gabriel Xavier <gabrielgrx@gmail.com>
2022-12-30 16:42:00 -0300

    Moving pieces

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,30 @@
 package application;
 
 import checkers.CheckersMatch;
+import checkers.CheckersPiece;
+import checkers.CheckersPosition;
+
+import java.util.Scanner;
 
 public class Program {
 
     public static void main(String[] args) {
 
+        Scanner sc = new Scanner(System.in);
         CheckersMatch checkersMatch = new CheckersMatch();
-        UI.printBoard(checkersMatch.getPieces());
+
+        while (true) {
+            UI.printBoard(checkersMatch.getPieces());
+            System.out.println();
+            System.out.print("Source: ");
+            CheckersPosition source = UI.readCheckersPosition(sc);
+
+            System.out.println();
+            System.out.print("Target: ");
+            CheckersPosition target = UI.readCheckersPosition(sc);
+
+            CheckersPiece capturedPiece = checkersMatch.performCheckersMove(source, target);
+        }
 
     }
 }
--- src/application/UI.java
+++ src/application/UI.java
@@ -1,7 +1,11 @@
 package application;
 
 import checkers.CheckersPiece;
+import checkers.CheckersPosition;
 import checkers.enums.Color;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class UI {
 
@@ -32,6 +36,18 @@ public class UI {
         System.out.flush();
     }
 
+    public static CheckersPosition readCheckersPosition(Scanner sc) {
+        try {
+            String s = sc.nextLine();
+            char column = s.charAt(0);
+            int row = Integer.parseInt(s.substring(1));
+            return new CheckersPosition(column, row);
+        } catch (RuntimeException e) {
+            throw new InputMismatchException("Error reading CheckersPosition. Valid values are from a1 to h8");
+        }
+    }
+
+
     public static void printBoard(CheckersPiece[][] pieces) {
         for (int i = 0; i < pieces.length; i++) {
             System.out.print((8 - i) + " ");
@@ -55,4 +71,5 @@ public class UI {
         }
         System.out.print(" ");
     }
+
 }
